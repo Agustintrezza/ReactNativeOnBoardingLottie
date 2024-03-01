3aa66b6e1117859b55e76a03bc9b8ae8fdb89e9c
Agustintrezza <agustin.trezza@gmail.com>
2024-02-29 21:22:30 -0300

    Segundo Commit

--- navigation/appNavigation.js
+++ navigation/appNavigation.js
@@ -1,54 +1,67 @@
-import React, {useEffect, useState} from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
-import OnBoardingScreen from '../screens/OnboardingScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { getItem } from '../utils/asyncStorage';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "../screens/HomeScreen";
+import OnBoardingScreen from "../screens/OnboardingScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { getItem } from "../utils/asyncStorage";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
+  const [showOnboarding, setShowOnboarding] = useState(null);
 
-    const [showOnboarding, setShowOnboarding] = useState(null)
+  useEffect(() => {
+    checkIfAlreadyOnboarded();
+  }, []);
 
-    useEffect(() => {
-        checkIfAlreadyOnboarded();
-    }, [])
-
-    const checkIfAlreadyOnboarded = async () => {
-        let onboarded = await getItem('onboarded');
-        if(onboarded == 1) {
-            // hide onboarding
-            setShowOnboarding(false)
-        } else {
-            //show onboarding
-            setShowOnboarding(true)
-        }
-    }
-    
-    if(showOnboarding== null) {
-        return null
-    }
-
-    if(showOnboarding) {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName='Onboarding'>
-                    <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnBoardingScreen}/>
-                    <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
+  const checkIfAlreadyOnboarded = async () => {
+    let onboarded = await getItem("onboarded");
+    if (onboarded == 1) {
+      // muestra onboarding
+      setShowOnboarding(false);
     } else {
-        return (
-            <NavigationContainer>
-                <Stack.Navigator initialRouteName='Home'>
-                    <Stack.Screen name="Onboarding" options={{headerShown: false}} component={OnBoardingScreen}/>
-                    <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}/>
-                </Stack.Navigator>
-            </NavigationContainer>
-        )
+      //oculta onboarding
+      setShowOnboarding(true);
     }
-    
+  };
+
+  if (showOnboarding == null) {
+    return null;
+  }
+
+  if (showOnboarding) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Onboarding">
+          <Stack.Screen
+            name="Onboarding"
+            options={{ headerShown: false }}
+            component={OnBoardingScreen}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Onboarding"
+            options={{ headerShown: false }}
+            component={OnBoardingScreen}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
